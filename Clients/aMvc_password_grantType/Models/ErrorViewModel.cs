c6ad8e6763e4b012525073248b09c5e127536080
HadyWafa <hady.wafa@hadafsolutions.com>
2023-07-22 20:15:06 +0300

    init

---
+++ Clients/aMvc_password_grantType/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aMvc_password_grantType.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
